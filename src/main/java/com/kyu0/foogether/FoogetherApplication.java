14306f1ba989d4c059fa5b33339164ff52f55b8d
Kyu0 <efftar@naver.com>
2022-04-28 21:12:59 +0900

    Initial Commit

---
+++ src/main/java/com/kyu0/foogether/FoogetherApplication.java
@@ -0,0 +1,13 @@
+package com.kyu0.foogether;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoogetherApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FoogetherApplication.class, args);
+	}
+
+}
