15b94508c34d3613535d3e378d64a554f043c64f
Kyu0 <efftar@naver.com>
2022-05-01 23:20:15 +0900

    [new] 비밀번호 찾기, 회원 가입 페이지 API 추가 비밀번호 찾기 - forgot, 회원 가입 - register

--- src/main/java/com/kyu0/foogether/IndexController.java
+++ src/main/java/com/kyu0/foogether/IndexController.java
@@ -6,8 +6,18 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class IndexController {
     
-    @GetMapping("/")
+    @GetMapping("/") // 로그인 페이지
     public String index() {
         return "index";
+    }
+
+    @GetMapping("/register") // 회원가입 페이지
+    public String register() {
+        return "register";
+    }
+
+    @GetMapping("/forgot") // 비밀번호 찾기 페이지
+    public String forgot() {
+        return "forgot";
     }
 }
